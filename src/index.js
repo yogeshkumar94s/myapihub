cb44571141942222a01c90fa6c763a625cb1efd8
Yogesh Kumar <yksainimbd@gmail.com>
2024-02-13 23:49:28 +0530

    app created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("index.js is running");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Could not connect to MongoDB Atlas", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello, E-commerce World!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
