058d272f28d1777afac43091d4e81cbe40805c74
Yogesh Kumar <yksainimbd@gmail.com>
2024-02-14 01:24:56 +0530

    mongoDB connection done

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-const app = express();
-
-app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB Atlas"))
-  .catch((err) => console.error("Could not connect to MongoDB Atlas", err));
-
-app.get("/", (req, res) => {
-  res.send("Hello, E-commerce World!");
+dotenv.config({
+  path: "./env",
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+connectDB();
